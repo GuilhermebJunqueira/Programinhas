5c321d9a7a866341dc5447e4cef6e9747b9b2955
GuilhermebJunqueira <143050492+GuilhermebJunqueira@users.noreply.github.com>
2023-09-29 16:50:48 -0300

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,14 @@
+def sao_equivalentes(conjunto1, conjunto2):
+    return conjunto1 == conjunto2
+
+entrada_conjunto1 = input("Digite os elementos do primeiro conjunto separados por vírgula: ")
+entrada_conjunto2 = input("Digite os elementos do segundo conjunto separados por vírgula: ")
+
+conjunto1 = set(entrada_conjunto1.split(','))
+conjunto2 = set(entrada_conjunto2.split(','))
+
+
+if conjunto1 == conjunto2
+    print("Os conjuntos são equivalentes.")
+else:
+    print("Os conjuntos não são equivalentes.")
